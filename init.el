ab2491e622178169126b1231cfd274721003f2c6
cooperL0 <cooperlenzi0@gmail.com>
2025-04-15 08:02:24 -0400

    added support for projectile

--- init.el
+++ init.el
@@ -85,6 +85,18 @@
       '((python-mode . python-ts-mode)))
 
 ;;**********************************************
+;; Projectile Setup
+;;**********************************************
+;;install ripgrep
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
+
+;;**********************************************
 ;; UTF Encoding, and etc
 ;;**********************************************
 
